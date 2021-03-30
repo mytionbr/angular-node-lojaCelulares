f39682dcd23371a5f5d8f83cfa074eaef416926b
mytionBr <matheuss.jesus2017@gmail.com>
2021-03-30 14:38:36 -0300

    added cell phone creation method

---
+++ backend/controllers/cellphone.controller.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const Cellphone = require('../models/cellphone.model')
+const router = express.Router()
+
+const createCellphone = async (req,res) =>{
+    
+    let cellphone = new Cellphone()
+    cellphone.name = req.body.name
+    cellphone.brand = req.body.brand
+    cellphone.price = req.body.price
+    cellphone.imageLink = req.body.imageLink
+    cellphone.colors = req.body.colors
+    cellphone.description = req.body.description
+    cellphone.memoryStorage = req.body.memoryStorage
+
+    try{
+        await cellphone.save()
+    }catch(e){
+        console.log(e)
+    }
+}
+
